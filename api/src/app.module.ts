035b09e17c1cd7a762967b9c566cc32e90976de5
wa-maro <theo.pombe@gmail.com>
2026-08-01 18:03:18 +0300

    setup(api/env): set global access to the enviroment variable with validation

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,7 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string()
+          .valid('development', 'production', 'test')
+          .default('development'),
+        PORT: Joi.number().port().default(3000),
+      }),
+      validationOptions: {
+        abortEarly: process.env.NODE_ENV === 'production',
+      },
+    }),
+  ],
   controllers: [],
   providers: [],
 })
--- api/src/main.ts
+++ api/src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const config = app.get(ConfigService);
+
+  const port = config.getOrThrow<number>('PORT') || 3000;
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -13,8 +18,8 @@ async function bootstrap() {
     }),
   );
 
-  app.setGlobalPrefix('api/v0/');
+  app.setGlobalPrefix('api/v1/');
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(port);
 }
 bootstrap();
