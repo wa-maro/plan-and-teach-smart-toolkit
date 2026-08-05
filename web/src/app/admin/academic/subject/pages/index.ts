6a406147d76475deadf250e0ebd79f1795ec76ad
wa-maro <theo.pombe@gmail.com>
2026-08-05 11:07:26 +0300

    feat(web/subject): add subject list page component

---
+++ web/src/app/admin/academic/subject/pages/index.ts
@@ -0,0 +1,1 @@
+export * from './subject-list/subject-list';
---
+++ web/src/app/admin/academic/subject/pages/subject-list/subject-list.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-subject-list',
+  imports: [],
+  templateUrl: './subject-list.html',
+  styles: ``,
+})
+export class SubjectList {}
