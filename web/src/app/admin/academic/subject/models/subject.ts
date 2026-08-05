0193bf5e53b22739e1fa7dcc94853ede5ab93ad3
wa-maro <theo.pombe@gmail.com>
2026-08-05 12:18:57 +0300

    feat(web/subject): define minimal and detailed subject model

---
+++ web/src/app/admin/academic/subject/models/subject.ts
@@ -0,0 +1,14 @@
+import { MediumOfInstructionMinimal } from '@preference/medium-of-instruction/models';
+
+export interface SubjectMinimal {
+  id: string;
+  name: string;
+}
+
+export interface Subject extends SubjectMinimal {
+  slug: string;
+  abbreviation: string;
+  mediumOfInstruction: MediumOfInstructionMinimal;
+  createdAt: Date;
+  updatedAt: Date;
+}
