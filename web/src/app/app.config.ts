0e8c8f7783caf737aefcbb69fcd0e6b6441c6ba5
wa-maro <theo.pombe@gmail.com>
2026-08-05 09:16:39 +0300

    feat(web/config): add typed environment configuration using injection token

--- web/src/app/app.config.ts
+++ web/src/app/app.config.ts
@@ -1,11 +1,26 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+  ApplicationConfig,
+  InjectionToken,
+  provideBrowserGlobalErrorListeners,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
+import { EnvConfig } from '@shared/types/config';
+
+export const ENV_CONFIG = new InjectionToken<EnvConfig>('env.config');
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+    provideRouter(routes),
+    provideHttpClient(),
+    {
+      provide: ENV_CONFIG,
+      useValue: {
+        apiUrl: 'http://localhost:3301/api/v1',
+        adminApiUrl: 'http://localhost:3301/api/v1/admin',
+      },
+    },
+  ],
 };
---
+++ web/src/app/shared/types/config/env.config.ts
@@ -0,0 +1,4 @@
+export interface EnvConfig {
+  apiUrl: string;
+  adminApiUrl: string;
+}
