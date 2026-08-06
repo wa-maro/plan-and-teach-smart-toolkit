91bfff465edfb8d2d658ae00e34d9027852d550e
wa-maro <theo.pombe@gmail.com>
2026-08-06 12:00:54 +0300

    feat(web/medium): fetch and render lists of media of instruction using SubjectTable

--- web/src/app/admin/preference/medium-of-instruction/pages/medium-list/medium-list.ts
+++ web/src/app/admin/preference/medium-of-instruction/pages/medium-list/medium-list.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { MediumService } from '@preference/medium-of-instruction/services';
+import { MediumTable } from '@preference/medium-of-instruction/components';
+import { MatIcon } from '@angular/material/icon';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-medium-list',
-  imports: [],
+  imports: [MediumTable, MatAnchor, MatIcon],
   templateUrl: './medium-list.html',
   styles: ``,
 })
-export class MediumList {}
+export class MediumList {
+  private readonly mediumService = inject(MediumService);
+  private readonly mediaResponse = toSignal(this.mediumService.findAll());
+
+  protected readonly media = computed(() => this.mediaResponse()?.data ?? []);
+}
