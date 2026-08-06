bea194ede148c95cc0f2cbf5df1080606b24e5da
wa-maro <theo.pombe@gmail.com>
2026-08-06 11:34:50 +0300

    feat(web/subject): fetch and render lists of subjects using SubjectTable

--- web/src/app/admin/academic/subject/pages/subject-list/subject-list.ts
+++ web/src/app/admin/academic/subject/pages/subject-list/subject-list.ts
@@ -1,9 +1,19 @@
-import { Component } from '@angular/core';
+import { SubjectService } from '@academic/subject/services';
+import { Component, computed, inject } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
+import { SubjectTable } from '@academic/subject/components';
+import { MatIcon } from '@angular/material/icon';
+import { MatAnchor } from '@angular/material/button';
 
 @Component({
   selector: 'app-subject-list',
-  imports: [],
+  imports: [SubjectTable, MatAnchor, MatIcon],
   templateUrl: './subject-list.html',
   styles: ``,
 })
-export class SubjectList {}
+export class SubjectList {
+  private readonly subjectService = inject(SubjectService);
+  private readonly subjectsResponse = toSignal(this.subjectService.findAll());
+
+  protected readonly subjects = computed(() => this.subjectsResponse()?.data ?? []);
+}
