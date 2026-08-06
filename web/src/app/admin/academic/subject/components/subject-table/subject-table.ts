03dc28d92df787c2e752b6f21d7a61ac4e076518
wa-maro <theo.pombe@gmail.com>
2026-08-06 13:12:06 +0300

    feat(web/tables): add reusable delete action to table components

--- web/src/app/admin/academic/subject/components/subject-table/subject-table.ts
+++ web/src/app/admin/academic/subject/components/subject-table/subject-table.ts
@@ -1,15 +1,28 @@
 import { Subject } from '@academic/subject/models';
 import { DatePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
+import { DeleteItemAction } from '../../../../core/components';
 
 @Component({
   selector: 'app-subject-table',
-  imports: [MatTableModule, DatePipe],
+  imports: [MatTableModule, DatePipe, DeleteItemAction],
   templateUrl: './subject-table.html',
   styles: ``,
 })
 export class SubjectTable {
   readonly subjects = input.required<Subject[]>();
-  protected readonly displayedColumns = ['index', 'name', 'mediumOfInstruction', 'updatedAt'];
+  protected readonly displayedColumns = [
+    'index',
+    'name',
+    'mediumOfInstruction',
+    'updatedAt',
+    'actions',
+  ];
+
+  protected readonly deleteSubject = output<Subject>();
+
+  protected onDelete(item: Subject) {
+    this.deleteSubject.emit(item);
+  }
 }
--- web/src/app/admin/preference/medium-of-instruction/components/medium-table/medium-table.ts
+++ web/src/app/admin/preference/medium-of-instruction/components/medium-table/medium-table.ts
@@ -1,15 +1,22 @@
 import { DatePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { MatTableModule } from '@angular/material/table';
 import { MediumOfInstruction } from '@preference/medium-of-instruction/models';
+import { DeleteItemAction } from '../../../../core/components';
 
 @Component({
   selector: 'app-medium-table',
-  imports: [MatTableModule, DatePipe],
+  imports: [MatTableModule, DatePipe, DeleteItemAction],
   templateUrl: './medium-table.html',
   styles: ``,
 })
 export class MediumTable {
   readonly media = input.required<MediumOfInstruction[]>();
-  protected readonly displayedColumns = ['index', 'name', 'updatedAt'];
+  protected readonly displayedColumns = ['index', 'name', 'updatedAt', 'actions'];
+
+  protected readonly deleteSubject = output<MediumOfInstruction>();
+
+  protected onDelete(item: MediumOfInstruction) {
+    this.deleteSubject.emit(item);
+  }
 }
