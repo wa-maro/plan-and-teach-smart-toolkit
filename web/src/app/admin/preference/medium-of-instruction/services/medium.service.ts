c9b317f876c27d8ecfd95df7d3ad267d4098ae69
wa-maro <theo.pombe@gmail.com>
2026-08-06 19:43:12 +0300

    feat(web/medium-of-instruction): implement medium-of-instruction domain service methods

--- web/src/app/admin/preference/medium-of-instruction/services/medium.service.ts
+++ web/src/app/admin/preference/medium-of-instruction/services/medium.service.ts
@@ -2,6 +2,8 @@ import { inject, Service } from '@angular/core';
 import { ENV_CONFIG } from '../../../../app.config';
 import { CrudService } from '../../../core/services';
 import { CreateMediumDto, MediumOfInstruction, UpdateMediumDto } from '../models';
+import { map, Observable } from 'rxjs';
+import { PaginatedResult } from '@shared/types/api';
 
 @Service()
 export class MediumService extends CrudService<
@@ -11,5 +13,30 @@ export class MediumService extends CrudService<
 > {
   constructor() {
     super(`${inject(ENV_CONFIG).adminApiUrl}/medium-of-instructions`);
+  }
+
+  getMedia(): Observable<PaginatedResult<MediumOfInstruction>> {
+    return this.apiFindMany().pipe(
+      map((response) => ({
+        data: response.data,
+        meta: response.meta,
+      })),
+    );
+  }
+
+  getMedium(id: string): Observable<MediumOfInstruction> {
+    return this.apiFindOne(id).pipe(map((response) => response.data));
+  }
+
+  createMedium(dto: CreateMediumDto): Observable<MediumOfInstruction> {
+    return this.apiCreate(dto).pipe(map((response) => response.data));
+  }
+
+  updateMedium(id: string, dto: UpdateMediumDto): Observable<MediumOfInstruction> {
+    return this.apiUpdate(id, dto).pipe(map((response) => response.data));
+  }
+
+  deleteMedium(id: string): Observable<void> {
+    return this.apiRemove(id).pipe(map(() => void 0));
   }
 }
