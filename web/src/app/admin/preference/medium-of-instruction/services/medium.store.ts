5e0135e965ee353e0119174f6307d1ccfb43be52
wa-maro <theo.pombe@gmail.com>
2026-08-07 09:21:23 +0300

    feat(web/medium):  implement media loading in MediumStore

--- web/src/app/admin/preference/medium-of-instruction/services/medium.store.ts
+++ web/src/app/admin/preference/medium-of-instruction/services/medium.store.ts
@@ -1,6 +1,7 @@
 import { computed, inject, Service, signal } from '@angular/core';
 import { MediumService } from './medium.service';
 import { MediumOfInstruction } from '../models';
+import { finalize } from 'rxjs';
 
 interface MediumState {
   media: MediumOfInstruction[];
@@ -19,6 +20,20 @@ export class MediumStore {
   readonly media = computed(() => this._state().media);
 
   readonly loading = computed(() => this._state().loading);
+
+  load(): void {
+    this.updateCurrentState({
+      loading: true,
+    });
+
+    this.mediumService
+      .getMedia()
+      .pipe(finalize(() => this.updateCurrentState({ loading: false })))
+      .subscribe({
+        next: ({ data }) => this.updateCurrentState({ media: data }),
+        error: (error) => console.error(error),
+      });
+  }
 
   private updateCurrentState(partial: Partial<MediumState>): void {
     this._state.update((state) => ({
