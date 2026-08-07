1b575dc633b07c4e13d1c85daf50090dd8782356
wa-maro <theo.pombe@gmail.com>
2026-08-07 10:02:00 +0300

    feat(web/medium): bind media state to medium table

--- web/src/app/admin/preference/medium-of-instruction/pages/medium-list/medium-list.ts
+++ web/src/app/admin/preference/medium-of-instruction/pages/medium-list/medium-list.ts
@@ -1,12 +1,14 @@
 import { MediumStore } from './../../services/medium.store';
 import { Component, inject } from '@angular/core';
+import { MediumTable } from '@preference/medium-of-instruction/components';
 import { MatIcon } from '@angular/material/icon';
 import { MatAnchor } from '@angular/material/button';
 import { MediumOfInstruction } from '@preference/medium-of-instruction/models';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-medium-list',
-  imports: [MatAnchor, MatIcon],
+  imports: [MediumTable, MatAnchor, MatIcon, MatProgressBar],
   templateUrl: './medium-list.html',
   styles: ``,
 })
