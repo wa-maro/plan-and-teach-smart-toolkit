f48b911fdfc04983e58a541ad5b1a1b0e524be7b
wa-maro <theo.pombe@gmail.com>
2026-08-07 17:03:42 +0300

    feat(api/medium): update detail medium response dto to include subjects

--- api/src/features/preference/medium-of-instructions/dtos/response/medium-response.dto.ts
+++ api/src/features/preference/medium-of-instructions/dtos/response/medium-response.dto.ts
@@ -1,15 +1,20 @@
 import { MediumOfInstruction } from '../../../../../generated/prisma/client';
+import { SubjectMinimalResponseDto } from '../../../../academic/subjects/dtos/response/subject-minima-response.tdo';
 import { MediumOfInstructionMinimalResponseDto } from './medium-minimal-response.dto';
 
 export class MediumOfInstructionResponseDto extends MediumOfInstructionMinimalResponseDto {
   readonly code: string;
+  readonly subjects?: SubjectMinimalResponseDto[];
   readonly createdAt: string;
   readonly updatedAt: string;
 
-  constructor(medium: MediumOfInstruction) {
+  constructor(medium: MediumOfInstruction, subjects?: any[]) {
     super(medium);
 
     this.code = medium.code;
+    this.subjects = subjects?.map(
+      (subj) => new SubjectMinimalResponseDto(subj),
+    );
     this.createdAt = medium.createdAt.toISOString();
     this.updatedAt = medium.updatedAt.toISOString();
   }
