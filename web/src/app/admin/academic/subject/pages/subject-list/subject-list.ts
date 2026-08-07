7507744f08f3748a05b85c5137c8da5f73a90fe4
wa-maro <theo.pombe@gmail.com>
2026-08-07 11:45:54 +0300

    feat(web/medium): load and bind subjects and loading state

--- web/src/app/admin/academic/subject/pages/subject-list/subject-list.ts
+++ web/src/app/admin/academic/subject/pages/subject-list/subject-list.ts
@@ -1,19 +1,27 @@
-import { SubjectService } from '@academic/subject/services';
-import { Component, computed, inject } from '@angular/core';
-import { toSignal } from '@angular/core/rxjs-interop';
+import { SubjectStore } from '@academic/subject/services';
+import { Component, inject } from '@angular/core';
 import { SubjectTable } from '@academic/subject/components';
 import { MatIcon } from '@angular/material/icon';
 import { MatAnchor } from '@angular/material/button';
+import { Subject } from '@academic/subject/models';
+import { MatProgressBar } from '@angular/material/progress-bar';
 
 @Component({
   selector: 'app-subject-list',
-  imports: [SubjectTable, MatAnchor, MatIcon],
+  imports: [SubjectTable, MatAnchor, MatIcon, MatProgressBar],
   templateUrl: './subject-list.html',
   styles: ``,
 })
 export class SubjectList {
-  private readonly subjectService = inject(SubjectService);
-  private readonly subjectsResponse = toSignal(this.subjectService.findAll());
+  private readonly subjectStore = inject(SubjectStore);
 
-  protected readonly subjects = computed(() => this.subjectsResponse()?.data ?? []);
+  protected readonly subjects = this.subjectStore.subjects;
+
+  protected readonly loading = this.subjectStore.subjects;
+
+  ngOnInit(): void {
+    this.subjectStore.load();
+  }
+
+  protected onDelete(subject: Subject) {}
 }
