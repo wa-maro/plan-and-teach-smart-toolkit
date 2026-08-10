2b27c8afff24a09a011830ecbfe3ab4f892f4acd
wa-maro <theo.pombe@gmail.com>
2026-08-10 20:17:11 +0300

    feat(web/subject): add subject detail component with subject form

---
+++ web/src/app/admin/academic/subject/components/subject-detail/subject-detail.ts
@@ -0,0 +1,34 @@
+import { Subject } from '@academic/subject/models';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
+import { SubjectForm } from '../subject-form/subject-form';
+import { MatDivider } from '@angular/material/divider';
+import { MatIcon } from '@angular/material/icon';
+import { DatePipe } from '@angular/common';
+import { SubjectStore } from '@academic/subject/services';
+import { MediumStore } from '@preference/medium-of-instruction/services';
+
+@Component({
+  selector: 'app-subject-detail',
+  imports: [SubjectForm, MatDivider, MatIcon, DatePipe],
+  templateUrl: './subject-detail.html',
+  styles: ``,
+})
+export class SubjectDetail implements OnInit {
+  private readonly subjectStore = inject(SubjectStore);
+  private readonly mediumStore = inject(MediumStore);
+
+  readonly selectedSubject = input.required<Subject>();
+
+  private readonly detailSubject = this.subjectStore.detailSubject;
+
+  protected readonly loading = this.subjectStore.detailLoading;
+
+  protected readonly subject = computed(() => this.detailSubject() ?? this.selectedSubject());
+
+  protected readonly mediaOptions = this.mediumStore.media;
+
+  ngOnInit(): void {
+    this.mediumStore.load();
+    this.subjectStore.detail(this.selectedSubject().id);
+  }
+}
