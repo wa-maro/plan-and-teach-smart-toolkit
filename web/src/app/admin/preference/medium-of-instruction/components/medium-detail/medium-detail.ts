2f1837a7c1c91008523f22a3ae8b89975f1dcb63
wa-maro <theo.pombe@gmail.com>
2026-08-10 19:54:27 +0300

    feat(web/medium): use medium store to sync detail component

--- web/src/app/admin/preference/medium-of-instruction/components/medium-detail/medium-detail.ts
+++ web/src/app/admin/preference/medium-of-instruction/components/medium-detail/medium-detail.ts
@@ -1,31 +1,40 @@
-import { Component, computed, input } from '@angular/core';
+import { Component, computed, inject, input, OnInit } from '@angular/core';
 import { MediumOfInstruction } from '@preference/medium-of-instruction/models';
-import { MatListModule } from '@angular/material/list';
+import { MatDivider } from '@angular/material/list';
 import { DatePipe } from '@angular/common';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
+import { MediumStore } from '@preference/medium-of-instruction/services';
 import { MediumForm } from '../medium-form/medium-form';
 import { MatIcon } from '@angular/material/icon';
 import { MatChipsModule } from '@angular/material/chips';
 import { RouterLink } from '@angular/router';
+import { MatProgressSpinner } from '@angular/material/progress-spinner';
 
 @Component({
   selector: 'app-medium-detail',
   imports: [
-    MatFormFieldModule,
-    MatInputModule,
-    MatListModule,
     DatePipe,
     MediumForm,
     MatIcon,
     MatChipsModule,
     RouterLink,
+    MatProgressSpinner,
+    MatDivider,
   ],
   templateUrl: './medium-detail.html',
   styles: ``,
 })
-export class MediumDetail {
+export class MediumDetail implements OnInit {
+  private readonly mediumStore = inject(MediumStore);
+
   readonly selectedMedium = input.required<MediumOfInstruction>();
 
-  protected readonly medium = computed(() => this.selectedMedium());
+  private readonly detailMedium = this.mediumStore.detailMedium;
+
+  protected readonly loading = this.mediumStore.detailLoading;
+
+  protected readonly medium = computed(() => this.detailMedium() ?? this.selectedMedium());
+
+  ngOnInit(): void {
+    this.mediumStore.detail(this.selectedMedium().id);
+  }
 }
