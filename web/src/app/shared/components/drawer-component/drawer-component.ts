9ab9c65f843445220fc9f5ddab5ed84f5b942fbf
wa-maro <theo.pombe@gmail.com>
2026-08-11 06:09:25 +0300

    feat(web/ui): add style to the app drawer component

--- web/src/app/shared/components/drawer-component/drawer-component.ts
+++ web/src/app/shared/components/drawer-component/drawer-component.ts
@@ -5,7 +5,25 @@ import { MatDrawer, MatDrawerContainer, MatDrawerContent } from '@angular/materi
   selector: 'app-drawer-component',
   imports: [MatDrawer, MatDrawerContent, MatDrawerContainer],
   templateUrl: './drawer-component.html',
-  styles: ``,
+  styles: `
+    .drawer-container {
+      height: calc(100vh - 70px);
+    }
+
+    mat-drawer {
+      width: 24rem;
+    }
+
+    mat-drawer-content {
+      background-color: rgb(248 250 252);
+    }
+
+    @media (max-width: 768px) {
+      mat-drawer {
+        width: min(85vw, 24rem);
+      }
+    }
+  `,
 })
 export class DrawerComponent {
   opened = model.required<boolean>();
