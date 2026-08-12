f1e5846beb79518910a9ce06d4e83178bcb7364b
wa-maro <theo.pombe@gmail.com>
2026-08-12 15:59:35 +0300

    feat(api/security): expose sessions service

--- api/src/security/security.module.ts
+++ api/src/security/security.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PasswordModule } from './password';
 import { JwtTokenModule } from './jwt-token';
+import { SessionsModule } from './sessions';
 
 @Module({
-  imports: [PasswordModule, JwtTokenModule],
-  exports: [PasswordModule, JwtTokenModule],
+  imports: [PasswordModule, JwtTokenModule, SessionsModule],
+  exports: [PasswordModule, JwtTokenModule, SessionsModule],
 })
 export class SecurityModule {}
---
+++ api/src/security/sessions/sessions.service.ts
@@ -0,0 +1,42 @@
+import { Injectable } from '@nestjs/common';
+import { Prisma, PrismaService } from '@prisma';
+
+@Injectable()
+export class SessionsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  private db(tx?: Prisma.TransactionClient) {
+    return tx ?? this.prisma;
+  }
+
+  async insertOne(
+    data: Prisma.SessionCreateInput,
+    tx?: Prisma.TransactionClient,
+  ) {
+    return this.db(tx).session.create({ data });
+  }
+
+  async findById(id: string) {
+    return this.prisma.session.findUnique({ where: { id } });
+  }
+
+  async findByIdWithUser(id: string, tx?: Prisma.TransactionClient) {
+    return this.db(tx).session.findUnique({
+      where: { id },
+      include: {
+        user: true,
+      },
+    });
+  }
+
+  async updateOne(
+    id: string,
+    data: Prisma.SessionUpdateInput,
+    tx?: Prisma.TransactionClient,
+  ) {
+    return this.db(tx).session.update({
+      where: { id },
+      data,
+    });
+  }
+}
