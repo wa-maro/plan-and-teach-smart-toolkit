f662c8897fedf5548e3c8bd463bc32ea725dac73
wa-maro <theo.pombe@gmail.com>
2026-08-12 17:41:55 +0300

    feat(api/common): add prisma exceptions mapper utility method

---
+++ api/src/common/utils/map-prisma-exception.ts
@@ -0,0 +1,28 @@
+import {
+  ConflictException,
+  HttpException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Prisma } from '@prisma';
+
+export function mapPrismaException(
+  exception: Prisma.PrismaClientKnownRequestError,
+): HttpException {
+  switch (exception.code) {
+    case 'P2002':
+      return new ConflictException(
+        'A resource with the provided value already exists',
+      );
+
+    case 'P2003':
+      return new ConflictException(
+        'Cannot complete this operation because the resource is referenced by another resource',
+      );
+
+    case 'P2025':
+      return new NotFoundException('Resource not found');
+
+    default:
+      return new HttpException('Database operation failed', 500);
+  }
+}
