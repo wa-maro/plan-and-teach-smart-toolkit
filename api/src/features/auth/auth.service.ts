774d8111b5188b64145f18b70e16aefdafeb7c28
wa-maro <theo.pombe@gmail.com>
2026-08-16 14:39:01 +0300

    feat(api/auth): implement local strategy for user authentication

--- api/src/features/auth/auth.service.ts
+++ api/src/features/auth/auth.service.ts
@@ -1,9 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { PrismaService } from '@app-prisma/service';
 import { UsersService } from '@users';
 import { JwtTokenService } from '@security/jwt-token';
 import { PasswordService } from '@security/password';
 import { SessionsService } from '@security/sessions';
+import { User } from '@app-prisma/client';
 
 @Injectable()
 export class AuthService {
@@ -14,4 +15,14 @@ export class AuthService {
     private readonly tokenService: JwtTokenService,
     private readonly sessionsService: SessionsService,
   ) {}
+
+  async validateUser(username: string, password: string): Promise<User> {
+    const user = await this.usersService.findByUsername(username);
+    if (!user) throw new UnauthorizedException('Invalid credentials');
+
+    const ok = await this.passwordService.compare(password, user.passwordHash);
+    if (!ok) throw new UnauthorizedException('Invalid credentials');
+
+    return user;
+  }
 }
---
+++ api/src/features/auth/strategies/local.strategy.ts
@@ -0,0 +1,16 @@
+import { User } from '@app-prisma/client';
+import { AuthService } from '@auth';
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class LocalStrategy extends PassportStrategy(Strategy, 'local') {
+  constructor(private readonly authService: AuthService) {
+    super();
+  }
+
+  async validate(username: string, password: string): Promise<User> {
+    return await this.authService.validateUser(username, password);
+  }
+}
