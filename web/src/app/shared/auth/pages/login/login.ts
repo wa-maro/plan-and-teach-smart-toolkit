7dd837980cc7ebc1655e4bfe3a963ae2cfe31417
wa-maro <theo.pombe@gmail.com>
2026-08-18 11:48:35 +0300

    feat(web/auth): implement functioning login page

--- web/src/app/shared/auth/pages/login/login.ts
+++ web/src/app/shared/auth/pages/login/login.ts
@@ -1,9 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { Router } from '@angular/router';
+import { LoginForm, LoginFormValue } from '@shared/auth/components';
+import { AuthStore } from '@shared/auth/stores';
+import { getDashboardRoute } from '@shared/auth/utils';
+import { ToastService } from '@shared/components/toast/service';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [LoginForm],
   templateUrl: './login.html',
   styles: ``,
 })
-export class Login {}
+export class Login {
+  private readonly authStore = inject(AuthStore);
+  private readonly toastService = inject(ToastService);
+  private readonly router = inject(Router);
+
+  protected readonly loading = this.authStore.loading;
+
+  protected submit(formValue: LoginFormValue): void {
+    this.authStore.login(formValue).subscribe({
+      next: (user) => {
+        this.toastService.success('Logged in successfully');
+
+        const goTo = getDashboardRoute(user.role);
+        this.router.navigate([goTo]);
+      },
+      error: () => {},
+    });
+  }
+}
